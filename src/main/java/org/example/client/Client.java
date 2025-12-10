4797a65173143b5a3854b72835901ba8ff0cbb77
Laura Dzikuc <laura.dzikuc@gmail.com>
2025-12-10 17:11:52 +0100

    klient łączy się z serwerem

--- src/main/java/org/example/client/Client.java
+++ src/main/java/org/example/client/Client.java
@@ -1,4 +1,25 @@
 package org.example.client;
+import java.io.IOException;
+import java.net.Socket;
 
-public class Client {
+public class Client{
+    private Socket socket;
+    public static void main(String[] args){
+        Client klient = new Client();
+        klient.polaczzserwerem("localhost", 6767);
+
+    }
+
+    public void polaczzserwerem(String host, int port){
+        try{
+            System.out.println("probojemy polaczyc sie z serwerem " + host + " : " + port);
+            socket=new Socket(host, port);
+            System.out.println("polaczono z serwerem :)");
+            socket.close();
+            System.out.println("koniec polaczenia");
+
+        } catch (IOException e){
+            System.out.println("blad poloczenia: " + e.getMessage());
+        }
+    }
 }
