b93634c5bd2c78a503fb853081c4f64f410d45bc
Laura Dzikuc <laura.dzikuc@gmail.com>
2026-01-16 16:27:37 +0100

    dodanie wyboru rozmiaru planszy

--- src/main/java/org/example/gui/BoardView.java
+++ src/main/java/org/example/gui/BoardView.java
@@ -5,24 +5,25 @@ import java.util.function.Consumer;
 
 public class BoardView extends GridPane {
 
-    public static final int ROZMIAR_PLANSZY = 19;
-
+    private final int rozmiar;
     private final Consumer<Field> obslugaKlikniecia;
     private StoneType nastepnyKamien = StoneType.BLACK;
 
-    public BoardView(Consumer<Field> obslugaKlikniecia) {
+    public BoardView(int rozmiar, Consumer<Field> obslugaKlikniecia) {
         this.obslugaKlikniecia = obslugaKlikniecia;
+        this.rozmiar =rozmiar;
         inicjalizujPlansze();
     }
 
     private void inicjalizujPlansze() {
-        for (int wiersz = 0; wiersz < ROZMIAR_PLANSZY; wiersz++) {
-            for (int kolumna = 0; kolumna < ROZMIAR_PLANSZY; kolumna++) {
+        getChildren().clear();
+
+        for (int wiersz = 0; wiersz < rozmiar; wiersz++) {
+            for (int kolumna = 0; kolumna <rozmiar; kolumna++) {
                 Field pole = new Field(wiersz, kolumna);
 
                 pole.setOnMouseClicked(e -> {
                     pole.ustawKamien(nastepnyKamien);
-
                     nastepnyKamien =
                         (nastepnyKamien == StoneType.BLACK)
                             ? StoneType.WHITE
--- src/main/java/org/example/gui/GameWindow.java
+++ src/main/java/org/example/gui/GameWindow.java
@@ -3,17 +3,52 @@ package org.example.gui;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.HBox;
+import javafx.scene.control.ComboBox;
 public class GameWindow {
 
     private final BorderPane korzen;
     private final Label pasekStatusu;
-    private final BoardView plansza;
+    private BoardView plansza;
 
     public GameWindow() {
         korzen = new BorderPane();
-        pasekStatusu = new Label("Status: kliknij pole");
-        plansza = new BoardView(pole -> {
+        pasekStatusu = new Label("Wybierz rozmiar planszy");
+
+        inicjalizujUklad();
+    }
+
+    private void inicjalizujUklad() {
+        ComboBox<String> wyborRozmiaru = new ComboBox<>();
+        wyborRozmiaru.getItems().addAll("19x19 (domyśln)", "13x13", "9x9");
+        wyborRozmiaru.setValue("19x19 (domyśln)");
+
+        wyborRozmiaru.setOnAction(e -> {
+            String wybor = wyborRozmiaru.getValue();
+
+            int rozmiar;
+            if (wybor.startsWith("9")) {
+                rozmiar = 9;
+            } else if (wybor.startsWith("13")) {
+                rozmiar = 13;
+            } else {
+                rozmiar = 19;
+            }
+            utworzNowaPlansze(rozmiar);
+            pasekStatusu.setText("Rozmiar planszy: " + rozmiar + "×" + rozmiar);
+        });
+
+        HBox gornyPanel = new HBox(10, new Label("Rozmiar planszy:"), wyborRozmiaru);
+        gornyPanel.setStyle("-fx-padding: 10;");
+
+        korzen.setTop(gornyPanel);
+        korzen.setBottom(pasekStatusu);
+
+        utworzNowaPlansze(19);
+    }
+
+    private void utworzNowaPlansze(int rozmiar) {
+        plansza = new BoardView(rozmiar, pole -> {
             pasekStatusu.setText(
                 "Kliknięto pole: (" +
                 pole.getWiersz() + ", " +
@@ -21,21 +56,10 @@ public class GameWindow {
             );
         });
 
-        inicjalizujUklad();
-    }
-
-    private void inicjalizujUklad() {
-        pasekStatusu.setStyle(
-            "-fx-border-color: pink;"+
-            "-fx-padding: 10;"
-        );
-
         korzen.setCenter(plansza);
-        korzen.setBottom(pasekStatusu);
     }
 
     public Parent getKorzen() {
         return korzen;
     }
 }
-
